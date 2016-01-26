77dffdecacc8b5e09111b73f694f49f7fd970c95
Jed Lund <jlund@xip.net>
2016-01-26 13:19:41 -0800

    v0.12.32

--- lib/DateTimeX/Format/Excel/Types.pm
+++ lib/DateTimeX/Format/Excel/Types.pm
@@ -1,5 +1,5 @@
 package DateTimeX::Format::Excel::Types;
-use version; our $VERSION = version->declare("v0.12.30");
+use version; our $VERSION = version->declare("v0.12.32");
 use	5.010;
 use strict;
 use warnings;
