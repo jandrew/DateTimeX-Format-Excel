94097b61a4d5c552833e17e6e1caad641de7dd61
Jed Lund <jlund@xip.net>
2016-02-02 11:52:04 -0800

    v0.12.34 	Updateing requirements to avoid testers fails

--- lib/DateTimeX/Format/Excel/Types.pm
+++ lib/DateTimeX/Format/Excel/Types.pm
@@ -1,5 +1,5 @@
 package DateTimeX::Format::Excel::Types;
-use version; our $VERSION = version->declare("v0.12.32");
+use version; our $VERSION = version->declare("v0.12.34");
 use	5.010;
 use strict;
 use warnings;
